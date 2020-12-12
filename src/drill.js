c650d139772f21b82e29feea9873c923a96a57cd
marmooo <marmooo@users.noreply.github.com>
2020-12-12 13:17:47 +0900

    fix typo

--- src/drill.js
+++ src/drill.js
@@ -744,7 +744,7 @@ function initQuery(mode) {
 // https://webinlet.com/2020/ios11以降でピンチインアウト拡大縮小禁止
 // 手を置いた時の誤爆を防ぎつつスクロールは許可
 function scrollEvent(e) {
-  if (!['MAIN', 'PLOBLEM-BOX', 'A', 'BUTTON'].includes(e.target.tagName)) {
+  if (!['MAIN', 'PROBLEM-BOX', 'A', 'BUTTON'].includes(e.target.tagName)) {
     e.preventDefault();
   }
 }
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2020-12-12 13:00';
+var CACHE_NAME = '2020-12-12 13:20';
 var urlsToCache = [
   '/touch-shuji/',
   '/touch-shuji/index.js',
