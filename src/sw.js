ac04e47e82cfe3263cd4b20793ee73e9f7380a9d
marmooo <marmooo@users.noreply.github.com>
2021-06-24 07:09:25 +0900

    bump bootstrap from 5.0.1 to 5.0.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-06-22 12:55';
+var CACHE_NAME = '2021-06-24 07:06';
 var urlsToCache = [
   '/touch-shuji/',
   '/touch-shuji/index.js',
@@ -11,7 +11,7 @@ var urlsToCache = [
   '/touch-shuji/mp3/incorrect1.mp3',
   '/touch-shuji/mp3/stupid5.mp3',
   '/touch-shuji/signature_pad.umd.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
