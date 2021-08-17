c3587baa887ecd9cb1e670bab98d3d0d1f12c8ca
marmooo <marmooo@users.noreply.github.com>
2021-08-18 07:28:05 +0900

    deno lint

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # タッチ習字ドリル
+
 [自動採点付きの習字ドリル](https://marmooo.github.io/touch-shuji/)です。
 
 ## Installation
+
 1. install [parsimonhi/animCJK](https://github.com/parsimonhi/animCJK)
-2. install [marmooo/graded-kanji-examples](https://github.com/marmooo/graded-kanji-examples) licensed under the [CC BY-SA 4.0](https://creativecommons.org/licenses/by/4.0/)
+2. install
+   [marmooo/graded-kanji-examples](https://github.com/marmooo/graded-kanji-examples)
+   licensed under the
+   [CC BY-SA 4.0](https://creativecommons.org/licenses/by/4.0/)
 
 ## Build
+
 ```
 bash build.sh
 ```
 
 ## Attribution
-- [parsimonhi/animCJK (kanji svg)](https://github.com/parsimonhi/animCJK) is licensed under the [Arphic Public License](https://www.freedesktop.org/wiki/Arphic_Public_License/).
-- [parsimonhi/animCJK (kana svg)](https://github.com/parsimonhi/animCJK) is licensed under the [LGPL-3.0](https://www.gnu.org/licenses/lgpl-3.0.html).
+
+- [parsimonhi/animCJK (kanji svg)](https://github.com/parsimonhi/animCJK) is
+  licensed under the
+  [Arphic Public License](https://www.freedesktop.org/wiki/Arphic_Public_License/).
+- [parsimonhi/animCJK (kana svg)](https://github.com/parsimonhi/animCJK) is
+  licensed under the [LGPL-3.0](https://www.gnu.org/licenses/lgpl-3.0.html).
--- src/sw.js
+++ src/sw.js
@@ -1,54 +1,53 @@
-var CACHE_NAME = '2021-08-12 08:10';
+var CACHE_NAME = "2021-08-09 00:08";
 var urlsToCache = [
-  '/touch-shuji/',
-  '/touch-shuji/index.js',
-  '/touch-shuji/drill.js',
-  '/touch-shuji/drill/',
-  '/touch-shuji/svg/eraser.svg',
-  '/touch-shuji/svg/dict.svg',
-  '/touch-shuji/mp3/correct1.mp3',
-  '/touch-shuji/mp3/correct3.mp3',
-  '/touch-shuji/mp3/incorrect1.mp3',
-  '/touch-shuji/mp3/stupid5.mp3',
-  '/touch-shuji/favicon/original.svg',
-  '/touch-shuji/signature_pad.umd.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
+  "/touch-shuji/",
+  "/touch-shuji/index.js",
+  "/touch-shuji/drill.js",
+  "/touch-shuji/drill/",
+  "/touch-shuji/svg/eraser.svg",
+  "/touch-shuji/svg/dict.svg",
+  "/touch-shuji/mp3/correct1.mp3",
+  "/touch-shuji/mp3/correct3.mp3",
+  "/touch-shuji/mp3/incorrect1.mp3",
+  "/touch-shuji/mp3/stupid5.mp3",
+  "/touch-shuji/favicon/original.svg",
+  "/touch-shuji/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      }),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      }),
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", function (event) {
   var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
