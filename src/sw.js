48bfad4720b5d262551ede3095c9db72d54a2f48
marmooo <marmooo@users.noreply.github.com>
2022-04-05 00:15:15 +0900

    bump signature_pad from 4.0.3 to 4.0.4

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-03 15:40';
+var CACHE_NAME = '2022-04-05 00:14';
 var urlsToCache = [
   "/touch-shuji/",
   "/touch-shuji/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/touch-shuji/mp3/incorrect1.mp3",
   "/touch-shuji/mp3/stupid5.mp3",
   "/touch-shuji/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.3/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
 ];
 
