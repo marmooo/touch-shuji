e37e3f752385095da9dcae642658c3fd561fb9a6
marmooo <marmooo@users.noreply.github.com>
2022-09-03 00:14:11 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -93,9 +93,9 @@ function setCleared() {
 }
 
 function shuffle(array) {
-  for (let i = array.length - 1; i >= 0; i--) {
-    const rand = Math.floor(Math.random() * (i + 1));
-    [array[i], array[rand]] = [array[rand], array[i]];
+  for (let i = array.length; 1 < i; i--) {
+    const k = Math.floor(Math.random() * i);
+    [array[k], array[i - 1]] = [array[i - 1], array[k]];
   }
   return array;
 }
