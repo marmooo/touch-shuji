a76ab026251c4bcb5382de860de2b80521e4d673
marmooo <marmooo@users.noreply.github.com>
2023-11-30 08:35:51 +0900

    improve

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ cp -r graded-kanji-examples/dist/* src/data
 mkdir -p docs
 cp -r src/* docs
 drop-inline-css -r src -o docs
+deno run -A bundle.js ./src/index.js > docs/index.js
+deno run -A bundle.js ./src/drill.js > docs/drill.js
 minify -r docs -o .
