2014cfb03e64fb2188b460bf60cf3c696babb76d
marmooo <marmooo@users.noreply.github.com>
2025-06-16 00:07:50 +0900

    bump @marmooo/kanji from 0.0.8 to 0.1.1

--- src/drill.js
+++ src/drill.js
@@ -1,5 +1,5 @@
-import { Kanji } from "https://cdn.jsdelivr.net/npm/@marmooo/kanji@0.0.8/esm/kanji.js";
-import { JKAT } from "https://cdn.jsdelivr.net/npm/@marmooo/kanji@0.0.8/esm/jkat.js";
+import { Kanji } from "https://cdn.jsdelivr.net/npm/@marmooo/kanji@0.1.1/esm/kanji.js";
+import { JKAT } from "https://cdn.jsdelivr.net/npm/@marmooo/kanji@0.1.1/esm/jkat.js";
 import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.0.7/+esm";
 
 const jkat = new Kanji(JKAT);
